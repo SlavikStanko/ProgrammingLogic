5d19d09b93a25675d432e55388cc16e8408f1609
SlavikStanko <slavikstanko03@gmail.com>
2025-08-03 04:09:29 +0000

    lab 5 done

---
+++ Lab5/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Lab5;
+
+
+class Animal
+{
+    // Virtual method 
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+// Step 2 Create Class: Cat
+class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: meow");
+    }
+}
+
+// Step 2 Create Class: Dog
+class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
+// Step 2 Create Class: My own Class: Cow
+class Cow : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cow says: moo moo");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 3 Test in Main
+
+        //  Animal object reference
+        Animal myAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();
+        Animal myCow = new Cow();
+
+        //  animalSound method on each object
+        myAnimal.animalSound();
+        myCat.animalSound();
+        myDog.animalSound();
+        myCow.animalSound();
+    }
+}
+
